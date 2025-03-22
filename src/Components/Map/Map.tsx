36e19365231aa67a0126ced4114470650f14d611
Abinashpatri07 <abinashpatri5@gmail.com>
2025-03-22 11:01:03 +0530

    Your commit message

--- src/Components/Map/Map.tsx
+++ src/Components/Map/Map.tsx
@@ -17,7 +17,7 @@ const Map: React.FC = () => {
 
       {/* Marker at Specific Location */}
       <Marker position={position}>
-        <Popup>📍 You are here! 😊</Popup>
+        <Popup>📍 You are here!</Popup>
       </Marker>
     </MapContainer>
   );
