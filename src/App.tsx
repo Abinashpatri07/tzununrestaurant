ff63d728161b445e1b1acd7f1a860261af8079dd
Abinashpatri07 <abinashpatri5@gmail.com>
2025-03-22 10:51:14 +0530

    Hlo

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ function App() {
     <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} /> */}
       </Routes>
       <Footer/>
     </BrowserRouter>
